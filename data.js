1405d105472b6feafc71ee1e544bc7fb030b5b2e
Jerry Reptak <jreptak@gmail.com>
2013-04-01 21:16:58 -0400

    Site v2.0

---
+++ data.js
@@ -0,0 +1,40 @@
+module.exports = {
+  jobs: [
+    {
+      title: "Front-end Researcher",
+      company: "Social Media Information Lab, Rutgers University",
+      desc: "I do social media research and its a load of fun! I'm the only front-end developer at the lab and it was the first front-end dev work I did. The current project I'm working on is called City Beat. It's an awesome project easily explained as \"Real-time Analytics for the City\". We're using Instagram and Twitter to find events (such as fires, concerts, sports games) from social media before journalists can! A past project I worked on is called <a href='http://sm.rutgers.edu/thebeat' target='_blank'>The Beat</a>. It mashed up Instagram pictures with the Google Streetview location of where that picture was taken. It went viral and I got some really awesome comments about it.",
+      time: "September 2012 - present",
+      link: "http://sm.rutgers.edu"
+    },
+    {
+      title: "DevOps Contractor",
+      company: "Peek",
+      desc: "My first project was to transition the old Java email attachment handling code to NodeJS. This was the job that got me into Node and Javascript, and now I use these for everything I do. My stack here was Node, AWS, Chef, Redis, mySQL. During the last month of working here I got the chance to create my own team and work on an Android game.",
+      time: "May 2012 - August 2012, November 2012",
+      link: "http://peek.ly"
+    },
+    {
+      title: "Student SysAdmin and Programmer",
+      company: "Laboratory for Computer Science Research, Rutgers University",
+      desc: "Maintained CentOS machines and computer labs for the undergrads & grads in computer science (1000+ users). Created custom Linux packages/programs that are requested by faculty or students.",
+      time: "April 2011 - May 2012",
+      link: "http://www.cs.rutgers.edu/lcsr/"
+    }
+  ],
+
+  projects: [
+    {
+      title: "Lorem Wisdom",
+      desc: "Lorem Ipsum generator except with text that has meaning. REST API for getting quotes, facts, random titles. Scraped 800+ sites for facts and quotes. Created in a few hours at hackRU 2013 with Bilal Quadri",
+      link: "",
+      github: "http://github.com/bilalq/JetQueue"
+    },
+    {
+      title: "GilterView",
+      desc: "",
+      link: "",
+      github: ""
+    }
+  ]
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
-
-/*
- * GET home page.
- */
+var data = require('../data');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', data);
 };
